b42ddcbf597dbeb707e77126d4619737bc53535e
shira <shiralifestyle@gmail.com>
2019-01-16 17:45:21 +0200

    added code to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-exports.default = defaultTask
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+      .pipe(sass({
+        outputStyle: 'expanded'
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  });
+   
+  gulp.task('sass:watch', function () {
+    gulp.watch('scss/*.scss', ['sass']);
+  });
+
+
+  gulp.task('default', function() {
+    gulp.run('sass');
+    gulp.run('sass:watch');
+  });
